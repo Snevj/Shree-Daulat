2f2967bb4418f941a83f6adb5fe4a777a156057b
Snevj <krishanagopal236606@gmail.com>
2025-01-10 17:31:21 +0530

    Adding the responsive design components

---
+++ frontend/src/pages/Gallery/gallery.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './gallery.css'
+
+const gallery = () => {
+  return (
+    <> 
+        <div className=''>
+
+        </div>
+    </>
+  )
+}
+
+export default gallery
