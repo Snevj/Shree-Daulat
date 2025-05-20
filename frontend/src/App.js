748934a334b5bccc4ac6f37c5fcfbdc26c1aee60
Snevj <krishanagopal236606@gmail.com>
2025-05-20 15:17:32 +0530

    Editing the gallery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import Layout from "./components/layouts/Layout";
 import React from "react";
+import Layout from "./components/layouts/Layout";
 import About from "./pages/About/about";
 import Work from "./pages/Work/experiencepast";
+import Gallery from "./pages/Gallery/gallery";
 import Contact from "./pages/Contact /contact";
 import ScrollToTop from "react-scroll-to-top";
 import MobileNav from "./components/mobileNav/mobileNav";
-import Gallery from "./pages/Gallery/gallery";
 
 
 function App() {
@@ -15,9 +15,9 @@ function App() {
       <Layout /> 
       <div className="container">
         <About/>
-        <Gallery />
-        <Work />
-        <Contact />
+        <Gallery/>
+        <Work/>
+        <Contact/>
       </div>
       <div className="footer mb-3 ms-3">
         <h4 className="text-center">For contacting, dial +91 8764019044</h4>
