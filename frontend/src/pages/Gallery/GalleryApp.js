2fd9c65a561433db9e7a5c479d6101122ed39c91
Snevj <krishanagopal236606@gmail.com>
2025-05-21 23:46:18 +0530

    Making the Gallery button functional

--- frontend/src/pages/Gallery/GalleryApp.js
+++ frontend/src/pages/Gallery/GalleryApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Lightbox from "yet-another-react-lightbox"
+import Lightbox from "yet-another-react-lightbox";
 import { useState } from "react";
 import { slides } from "./data";
 import "yet-another-react-lightbox/styles.css";
@@ -8,32 +8,34 @@ import "yet-another-react-lightbox/plugins/captions.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import Images from './Images';
 
-
-
 function App() {
-
-  // const [open, setOpen] = useState<boolean>(false);
-  const [index, setIndex] = useState(-1)
+  // Remove the TypeScript notation
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(-1);
+  
   return (
     <>
-      {/* <button onClick={() => setOpen(true)}>Open Lightbox</button> */}
-      <Images data={slides} onClick={ (currentIndex) => setIndex(currentIndex) }/>
+      <button onClick={() => setOpen(true)}>Open Lightbox</button>
+      <Images data={slides} onClick={(currentIndex) => setIndex(currentIndex)} />
       
-      <Lightbox 
-      plugins={[Captions, Fullscreen, Thumbnails]} 
-      captions={{
-        showToggle: true,
-        descriptionTextAlign: 'end',
-      }}
-      // close={() => setOpen(false)}        
-      // open={open} 
-      index={index} 
-      open= { index>= 0}
-      close={()=> setIndex(-1)}
-      slides={slides} 
+      <Lightbox
+        plugins={[Captions, Fullscreen, Thumbnails]}
+        captions={{
+          showToggle: true,
+          descriptionTextAlign: 'end',
+        }}
+        close={() => setOpen(false)}
+        open={open || index >= 0}
+        index={index}
+        slides={slides}
+        
+        onClose={() => {
+          setOpen(false);
+          setIndex(-1);
+        }}
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
