693438784783b481069b1b2861a78272077737f1
Snevj <krishanagopal236606@gmail.com>
2025-05-26 01:18:11 +0530

    Adding some animations in the CSS File of Menus and also adjusting the code of menus.js

--- frontend/src/components/Menus/Menus.js
+++ frontend/src/components/Menus/Menus.js
@@ -1,115 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Menus.css";
 import { Link } from 'react-scroll';
 import { MdHome } from "react-icons/md"
-import { IoMdPhotos } from "react-icons/io";//all next 4 imports are of icons from react icons
+import { IoMdPhotos } from "react-icons/io";
 import { IoIosInformationCircleOutline } from "react-icons/io";
 import { MdWork } from "react-icons/md";
 import { IoMdContact } from "react-icons/io";
-import image from "../../assets/images/Shree daulat.png" //I have imported the image from the Menus folder
+import image from "../../assets/images/Shree daulat.png"
 
+const Menus = ({ toggle }) => {
+    const [activeLink, setActiveLink] = useState('home');
 
+    // Navigation items data
+    const navItems = [
+        {
+            id: 'home',
+            icon: <MdHome />,
+            label: 'Home',
+            target: 'home'
+        },
+        {
+            id: 'about',
+            icon: <IoIosInformationCircleOutline />,
+            label: 'About Us',
+            target: 'about'
+        },
+        {
+            id: 'gallery',
+            icon: <IoMdPhotos />,
+            label: 'Gallery',
+            target: 'gallery'
+        },
+        {
+            id: 'work',
+            icon: <MdWork />,
+            label: 'Past Work',
+            target: 'experiencepast'
+        },
+        {
+            id: 'contact',
+            icon: <IoMdContact />,
+            label: 'Contact',
+            target: 'contact'
+        }
+    ];
 
-const Menus = ({toggle}) => {
-  return (
-    <>
-    {toggle ? (
-        <>
-        <div className='nav-container'>
-            <div className= "navbar-profile-pic">
-                <img src={image} alt="Profile Pic" style={{ width: "150px", height: "auto"  }}/>
-            </div>
-             {/* Profile Picture Section */}
+    const handleLinkClick = (linkId) => {
+        setActiveLink(linkId);
+    };
+
+    return (
+        <div className={`nav-container ${toggle ? 'expanded' : 'contracted'}`}>
+            {toggle && (
+                <div className="navbar-profile-pic">
+                    <img 
+                        src={image} 
+                        alt="Profile Pic" 
+                        style={{ width: "150px", height: "auto" }}
+                    />
+                </div>
+            )}
+            
             <div className='nav-items'>
-                <div className='nav-item'>{/*All the nav-link are closed in nav-item*/}
-                    <div className='nav-link'>
-                        <Link to="home" spy={true} smooth={true} offset={-100} duration={100}>
-                            <MdHome/>
-                            Home
-                        </Link>            
+                {navItems.map((item) => (
+                    <div key={item.id} className='nav-item'>
+                        <div 
+                            className={`nav-link ${activeLink === item.id ? 'active' : ''}`}
+                            data-tooltip={item.label}
+                            onClick={() => handleLinkClick(item.id)}
+                        >
+                            <Link 
+                                to={item.target} 
+                                spy={true} 
+                                smooth={true} 
+                                offset={-100} 
+                                duration={500}
+                                onSetActive={() => setActiveLink(item.id)}
+                            >
+                                {item.icon}
+                                {toggle && <span>{item.label}</span>}
+                            </Link>
+                        </div>
                     </div>
-                </div>
-                <div className='nav-item'>
-                    <div className='nav-link'>
-                        <Link to="about" spy={true} smooth={true} offset={-100} duration={100}>
-                            <IoIosInformationCircleOutline/>
-                            About Us
-                        </Link>                    
-                    </div>
-                </div>
-                <div className='nav-item'>
-                    <div className='nav-link'>
-                        <Link to="gallery" spy={true} smooth={true} offset={-100} duration={100}>
-                            <IoMdPhotos />
-                            Gallery
-                        </Link>                    
-                    </div>
-                </div>
-                <div className='nav-item'>
-                    <div className='nav-link'>
-                        <Link to="experiencepast" spy={true} smooth={true} offset={-100} duration={100}>
-                            <MdWork/>
-                            Past Work
-                        </Link>
-                    </div>
-                </div>
-                <div className='nav-item'>
-                    <div className='nav-link'>
-                        <Link to="contact" spy={true} smooth={true} offset={-100} duration={100}>
-                            <IoMdContact/>
-                            Contact
-                        </Link>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
-        </>
-    ) : (
-        <div className='nav-items'>
-            <div className='nav-item'>
-                <div className='nav-link'>
-                    <Link to="home" spy={true} smooth={true} offset={-100} duration={100}>
-                        <MdHome/>
-                        
-                    </Link>            
-                </div>
-            </div>
-            <div className='nav-item'>
-                <div className='nav-link'>
-                    <Link to="about" spy={true} smooth={true} offset={-100} duration={100}>
-                        <IoIosInformationCircleOutline/>
-                        
-                    </Link>                    
-                </div>
-            </div>
-            <div className='nav-item'>
-                <div className='nav-link'>
-                    <Link to="gallery" spy={true} smooth={true} offset={-100} duration={100}>
-                        <IoMdPhotos />
-                        
-                    </Link>                    
-                </div>
-            </div>
-            <div className='nav-item'>
-                <div className='nav-link'>
-                    <Link to="experiencepast" spy={true} smooth={true} offset={-100} duration={100}>
-                        <MdWork/>
+    );
+};
 
-                    </Link>
-                </div>
-            </div>
-            <div className='nav-item'>
-                <div className='nav-link'>
-                    <Link to="contact" spy={true} smooth={true} offset={-100} duration={100}>
-                        <IoMdContact/>
-                        
-                    </Link>
-                </div>
-            </div>
-        </div>
-    )}
-    </>
-  )
-}
-
-export default Menus
+export default Menus;
